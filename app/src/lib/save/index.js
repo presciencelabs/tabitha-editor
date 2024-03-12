d72eb916941fe52ca5eac352123416f7b4503b31
Billy Clark <billy.clark@presciencelabs.org>
2024-03-12 11:31:45 -0400

    added ability to save entered text to localstorage (#66)
    Co-authored-by: billy clark <billy_clark@presciencelabs.org>

---
+++ app/src/lib/save/index.js
@@ -0,0 +1,31 @@
+import { browser } from '$app/environment'
+import SaveButton from './SaveButton.svelte'
+import { writable } from 'svelte/store'
+
+const saved = init()
+
+/** @returns {import('svelte/store').Writable<string>} */
+function init() {
+	const store = writable('', set => {
+		const value = browser && localStorage?.getItem('saved') || ''
+		set(value)
+		console.log('const: ', value)
+	})
+
+	const { subscribe, set, update } = store
+
+	return {
+		subscribe,
+		set: value => {
+			browser && localStorage.setItem('saved', value)
+			set(value)
+			console.log('set: ', value)
+		},
+		update,
+	}
+}
+
+export {
+	saved,
+	SaveButton,
+}
