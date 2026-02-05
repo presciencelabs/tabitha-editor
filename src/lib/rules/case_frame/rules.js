dabe3b7dfb38d0b5b3e8c270fbdd62d1d85c1b00
craigp_atw <craig_palmer@alltheword.org>
2026-02-05 08:39:35 -0800

    Fix: use ALL present case frame arguments, not just valid ones

--- src/lib/rules/case_frame/rules.js
+++ src/lib/rules/case_frame/rules.js
@@ -106,7 +106,8 @@ const argument_and_sense_rules = [
 
 				// An adjective being used predicatively should be tagged as such so the verb case frame rules can check it.
 				// A verse reference is another special case that should not interfere with Verb case frames.
-				const present_arguments = selected_result.case_frame.result.valid_arguments.map(arg => arg.role_tag)
+				const case_frame = selected_result.case_frame.result
+				const present_arguments = [...case_frame.valid_arguments, ...case_frame.extra_arguments].map(arg => arg.role_tag)
 				if (!(present_arguments.includes('modified_noun') || present_arguments.includes('modified_noun_with_subgroup'))
 						&& !token_has_tag(token, { 'role': 'verse_ref' })) {
 					add_tag_to_token(token, { 'adj_usage': 'predicative' }, trigger_context.rule_id)
